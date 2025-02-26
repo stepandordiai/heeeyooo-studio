e0ee2a220d95b7fc6db2852a39e1e535e9d03a21
Stepan Dordiai <stepandordiai@gmail.com>
2025-02-26 17:46:29 +0100

    v1.5.1 Update Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,13 @@
-import "./Header.scss";
-import logo from "./../../assets/logo/heeeyooo-studio-logo-white-v1.svg";
 import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import logo from "./../../assets/logo/heeeyooo-studio-logo-white-v1.svg";
+import "./Header.scss";
 
 const Header = () => {
     useEffect(() => {
         document.querySelector(".menu-txt").addEventListener("click", () => {
             document.querySelector(".menu").classList.toggle("menu--active");
             document.querySelector(".nav").classList.toggle("nav--active");
-            // document.querySelectorAll(".text-line").forEach((el, index) => {
-            // setTimeout(() => {
-            // el.style.animation = "revealTextLine 1s forwards";
-            // el.classList.toggle("text-line--active");
-            // }, 300 * index);
-            // });
         });
 
         document.querySelector(".js-link").addEventListener("click", () => {
@@ -23,41 +17,23 @@ const Header = () => {
     }, []);
 
     return (
-        <>
-            <header className="header">
-                <div className="header__logo">
-                    <img src={logo} alt="" />
-                    <span>heeeyooo.studio</span>
-                </div>
-                <div className="menu">
-                    <div className="menu-txt">menu</div>
-                    <nav className="nav">
-                        <div className="text-line-wrapper">
-                            <div className="text-line">
-                                <p>Home</p>
-                            </div>
-                        </div>
-                        <div className="text-line-wrapper">
-                            <div className="text-line">
-                                <p>About</p>
-                            </div>
-                        </div>
-                        <div className="text-line-wrapper">
-                            <div className="text-line">
-                                <NavLink className={"js-link"} to="/projects">
-                                    Projects
-                                </NavLink>
-                            </div>
-                        </div>
-                        <div className="text-line-wrapper">
-                            <div className="text-line">
-                                <p>Contact</p>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </header>
-        </>
+        <header className="header">
+            <div className="header__logo">
+                <img src={logo} alt="Logo" />
+                <span>heeeyooo.studio</span>
+            </div>
+            <div className="menu">
+                <div className="menu-txt">menu</div>
+                <nav className="nav">
+                    <p>Home</p>
+                    <p>About</p>
+                    <NavLink className={"js-link"} to="/projects">
+                        Projects
+                    </NavLink>
+                    <p>Contact</p>
+                </nav>
+            </div>
+        </header>
     );
 };
 
