90cab57ae19c25ba872b4f6c5bdcf5d5d9d8ffe0
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-19 20:21:21 +0200

    v1.0.6 Update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,51 +5,89 @@ import styles from "./../styles/Header.module.scss";
 import { useRef } from "react";
 
 const Header = () => {
-    const menu = useRef<HTMLDivElement>(null);
-    const nav = useRef<HTMLElement>(null);
+	const menu = useRef<HTMLDivElement>(null);
+	const nav = useRef<HTMLElement>(null);
 
-    const toggleMenu = () => {
-        if (menu.current && nav.current) {
-            menu.current.classList.toggle(styles.menu__active);
-            nav.current.classList.toggle(styles.nav__active);
-        }
-    };
+	const toggleMenu = () => {
+		const wordLineElements = document.querySelectorAll(
+			`.${styles["word-line"]}`
+		);
 
-    function hideMenu() {
-        document.querySelector(".menu")!.classList.remove("menu--active");
-        document.querySelector(".nav")!.classList.remove("nav--active");
-    }
+		if (menu.current && wordLineElements.length > 0) {
+			menu.current.classList.toggle(styles.menu__active);
+			wordLineElements.forEach((el, index) => {
+				setTimeout(() => {
+					el.classList.toggle(styles["word-line--active"]);
+				}, index * 100);
+			});
+		}
+	};
 
-    return (
-        <header className={`${styles.header}`}>
-            <div className={`${styles.header__logo}`}>
-                <Image
-                    className={styles.logo__img}
-                    src={logo}
-                    layout="responsive"
-                    width={50}
-                    height={50}
-                    alt="Logo"
-                />
-                <span className="js-scrambled-txt">heeeyooo.studio</span>
-            </div>
-            <div ref={menu} className={`${styles.menu}`}>
-                <div
-                    onClick={toggleMenu}
-                    className={`${styles.menu__txt} js-scrambled-txt`}
-                >
-                    Menu
-                </div>
-                <nav ref={nav} className={`${styles.nav}`}>
-                    <Link onClick={hideMenu} className={"js-link"} href={"/"}>
-                        Home
-                    </Link>
-                    <p>About</p>
-                    <p>Contact</p>
-                </nav>
-            </div>
-        </header>
-    );
+	function hideMenu() {
+		const wordLineElements = document.querySelectorAll(
+			`.${styles["word-line"]}`
+		);
+		if (menu.current && wordLineElements.length > 0) {
+			menu.current.classList.remove(styles.menu__active);
+			wordLineElements.forEach((el) => {
+				el.classList.remove(styles["word-line--active"]);
+			});
+		}
+	}
+
+	return (
+		<header className={`${styles.header}`}>
+			<div className={`${styles.header__logo}`}>
+				<Image
+					className={styles.logo__img}
+					src={logo}
+					layout="responsive"
+					width={50}
+					height={50}
+					alt="Logo"
+				/>
+				<span className="js-scrambled-txt">heeeyooo.studio</span>
+			</div>
+			<div ref={menu} className={`${styles.menu}`}>
+				<div
+					onClick={toggleMenu}
+					className={`${styles.menu__txt} js-scrambled-txt`}
+				>
+					Menu
+				</div>
+				<nav ref={nav} className={`${styles.nav}`}>
+					<div className={styles["word-line__container"]}>
+						<div className={styles["word-line"]}>
+							<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
+								Home
+							</Link>
+						</div>
+					</div>
+					<div className={styles["word-line__container"]}>
+						<div className={styles["word-line"]}>
+							<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
+								About
+							</Link>
+						</div>
+					</div>
+					<div className={styles["word-line__container"]}>
+						<div className={styles["word-line"]}>
+							<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
+								Projects
+							</Link>
+						</div>
+					</div>
+
+					{/* <Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
+						About
+					</Link>
+					<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
+						Projects
+					</Link> */}
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
