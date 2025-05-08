a05539c6b0c6c92c035e5c9c093345ee6dd05dd1
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-08 09:33:09 +0200

    v1.0.7 Update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,90 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "./../public/logo/heeeyooo-studio-logo-white-v1.svg";
+import logoImg from "./../public/logo/heeeyooo-studio-logo-white-v1.svg";
 import styles from "./../styles/Header.module.scss";
-import { useRef } from "react";
 
 const Header = () => {
-	const menu = useRef<HTMLDivElement>(null);
-	const nav = useRef<HTMLElement>(null);
-
-	const toggleMenu = () => {
-		const wordLineElements = document.querySelectorAll(
-			`.${styles["word-line"]}`
-		);
-
-		if (menu.current && wordLineElements.length > 0) {
-			menu.current.classList.toggle(styles.menu__active);
-			wordLineElements.forEach((el, index) => {
-				setTimeout(() => {
-					el.classList.toggle(styles["word-line--active"]);
-				}, index * 100);
-			});
-		}
-	};
-
-	function hideMenu() {
-		const wordLineElements = document.querySelectorAll(
-			`.${styles["word-line"]}`
-		);
-		if (menu.current && wordLineElements.length > 0) {
-			menu.current.classList.remove(styles.menu__active);
-			wordLineElements.forEach((el) => {
-				el.classList.remove(styles["word-line--active"]);
-			});
-		}
-	}
-
 	return (
-		<header className={`${styles.header}`}>
-			<div className={`${styles.header__logo}`}>
-				<Image
-					className={styles.logo__img}
-					src={logo}
-					layout="responsive"
-					width={50}
-					height={50}
-					alt="Logo"
-				/>
-				<span className="js-scrambled-txt">heeeyooo.studio</span>
-			</div>
-			<div ref={menu} className={`${styles.menu}`}>
-				<div
-					onClick={toggleMenu}
-					className={`${styles.menu__txt} js-scrambled-txt`}
-				>
-					Menu
-				</div>
-				<nav ref={nav} className={`${styles.nav}`}>
-					<div className={styles["word-line__container"]}>
-						<div className={styles["word-line"]}>
-							<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
-								Home
-							</Link>
-						</div>
-					</div>
-					<div className={styles["word-line__container"]}>
-						<div className={styles["word-line"]}>
-							<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
-								About
-							</Link>
-						</div>
-					</div>
-					<div className={styles["word-line__container"]}>
-						<div className={styles["word-line"]}>
-							<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
-								Projects
-							</Link>
-						</div>
-					</div>
-
-					{/* <Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
-						About
-					</Link>
-					<Link onClick={hideMenu} className={styles["js-link"]} href={"/"}>
-						Projects
-					</Link> */}
-				</nav>
+		<header className={styles.header}>
+			<div className={styles["header-top"]}>
+				<Link href={"/"} className={styles["header__logo-link"]}>
+					<Image
+						className={styles["header__logo-img"]}
+						src={logoImg}
+						layout="responsive"
+						height={30}
+						alt=""
+					/>
+					<span>heeeyooo.studio</span>
+				</Link>
+				<button className={styles["menu-btn"]}>
+					<span>menu</span>
+				</button>
 			</div>
 		</header>
 	);
